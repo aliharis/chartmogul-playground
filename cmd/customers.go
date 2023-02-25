9845924a3cc5f684a98e4042ba7658f7b9042200
Ali Haris <haris@kodefly.com>
2023-02-25 15:29:38 +0000

    feat: add a command to assign sales rep to each customer (random)

--- cmd/customers.go
+++ cmd/customers.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"encoding/csv"
 	"fmt"
+	"math/rand"
 	"os"
 
 	"github.com/aliharis/chartmogul-playground/utils/chartmogul"
@@ -77,7 +79,84 @@ var customersDeleteCmd = &cobra.Command{
 	},
 }
 
+var customersSetSalesRepCmd = &cobra.Command{
+	Use:   "customers:set-sales-rep",
+	Short: "Read a CSV file with the sales rep data and set the sales rep for each customer.",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Get the file path from the flag
+		filePath := cmd.Flag("file").Value.String()
+
+		// open the CSV file
+		file, err := os.Open(filePath)
+		if err != nil {
+			fmt.Println("Error opening file:", err)
+			return
+		}
+		defer file.Close()
+
+		// create a new CSV reader
+		reader := csv.NewReader(file)
+
+		// read the header row and make sure it has "Name" and "External ID" fields
+		header, err := reader.Read()
+		if err != nil {
+			fmt.Println("Error reading CSV file:", err)
+			return
+		}
+
+		// TODO run a check to make sure the header has the required fields
+
+		// map the header columns to struct fields
+		fields := make(map[string]int)
+		for i, name := range header {
+			fields[name] = i
+		}
+
+		// Read the sales rep name from CSV and put it in a slice
+		var salesReps []string
+		for {
+			// read a row
+			row, err := reader.Read()
+			if err != nil {
+				break
+			}
+
+			// get the sales rep name
+			salesRep := row[fields["Name"]]
+			salesReps = append(salesReps, salesRep)
+		}
+
+		// Get all the customers from the API
+		var api = chartmogul.GetAPI()
+		customers, err := api.ListCustomers(&cm.ListCustomersParams{})
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		// Iterate over the customers
+		for _, customer := range customers.Entries {
+			// Get a random sales rep name
+			salesRep := salesReps[rand.Intn(len(salesReps))]
+
+			// Set the sales rep for the customer
+			api.AddCustomAttributesToCustomer(customer.UUID,
+				[]*cm.CustomAttribute{
+					{
+						Type:  "String",
+						Key:   "salesRep",
+						Value: salesRep},
+				})
+
+			fmt.Println(salesRep, " has been set for customer", customer.Name)
+		}
+	},
+}
+
 func init() {
 	rootCmd.AddCommand(customersDeleteCmd)
 	rootCmd.AddCommand(customersListCmd)
+
+	// Customer Set Sales Rep
+	rootCmd.AddCommand(customersSetSalesRepCmd)
+	customersSetSalesRepCmd.PersistentFlags().String("file", "", "Path to the sales rep file.")
 }
